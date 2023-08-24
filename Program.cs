c01b5ea7da2bd8e878f45080d2060078675449ad
MarufUsmonovdev <marufusmonov1996@gmail.com>
2023-08-24 16:55:17 +0500

    Teach-Calculate

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Radiusni kiriting: ");
+int r = Convert.ToInt32(Console.ReadLine());
+
+decimal L = 2*3.14m*r;
+decimal S = 3.14m*(r*r);
+System.Console.WriteLine(L);
+System.Console.WriteLine(S);
